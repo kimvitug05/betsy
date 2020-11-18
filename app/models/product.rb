2aea67d54e8dc9cc94817194a28c2c59747d03cd
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-17 18:18:49 -0800

    add relations to products model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :merchant
+  has_many :reviews
+  has_many :categorizations
+  has_many :order_items
+  has_many :orders, through: :order_items
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
