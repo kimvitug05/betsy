4c71829c9335958bd4caf6b8d81d280346a5ccb6
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-17 17:17:38 -0800

    add product model validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  belongs_to :merchant
+
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
