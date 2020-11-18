05bc0e340fa87ba5aae0ba5908b8899375ff1de9
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-18 15:14:46 -0800

    add oauth routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   get 'product/destroy'
   get 'merchants/index'
   get 'merchants/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmlget "/auth/github", as: "github_login" #OmniAuth login
+  get "/auth/github", as: "github_login" #OmniAuth login
+  get "/auth/:provider/callback", to: "users#create" #OmniAuth Github callback
+  post "/logout", to: "users#logout", as: "logout"
 
   resources :merchants
   resources :products
