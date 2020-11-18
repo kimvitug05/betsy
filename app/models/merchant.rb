74e2c92768a7fb8cf2a5432f91d3c5ac1538b7b2
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-17 17:08:03 -0800

    add merchant model validations

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,4 +1,4 @@
 class Merchant < ApplicationRecord
   validates :username, uniqueness: true, presence: true
-  validates :email, presence: true, format: {with: /@/}
+  validates :email, presence: true, format: {with: /@/}, uniqueness: true
 end
