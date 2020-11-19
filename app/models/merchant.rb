ff31b91e7997cabc7bfb078fea37af9104d532ba
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-18 16:07:07 -0800

    Add build_from_github method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,15 @@ class Merchant < ApplicationRecord
 
   validates :username, uniqueness: true, presence: true
   validates :email, presence: true, format: {with: /@/}, uniqueness: true
+
+  def self.build_from_github(auth_hash)
+    user = Merchant.new
+    user.uid = auth_hash[:uid]
+    user.provider = "github"
+    user.username = auth_hash["info"]["name"]
+    user.email = auth_hash["info"]["email"]
+    # Note that the user has not been saved.
+    # We'll choose to do the saving outside of this method
+    return user
+  end
 end
