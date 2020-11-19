578a100d2c65f118469fd0c5b6ae9c93542b70ca
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-18 16:59:46 -0800

    edit flash messages for merchants controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -5,17 +5,24 @@ class MerchantsController < ApplicationController
     merchant = Merchant.find_by(uid: auth_hash[:uid], provider: "github")
 
     if merchant
-      flash[:success] = "Logged in as returning user #{merchant.username}"
+      flash[:status] = :success
+      flash[:result_text] = "Existing user #{merchant.username} is logged in."
     else
       merchant = Merchant.build_from_github(auth_hash)
 
       if merchant.save
-        flash[:success] = "Logged in as new user #{merchant.username}"
+        flash[:status] = :success
+        flash[:result_text] = "Logged in as new user #{merchant.username}"
       else
-        flash[:error] = "Could not create new user account: #{merchant.errors.messages}"
-        return redirect_to root_path
+        flash[:status] = :failure
+        flash[:result_text] = "Could not create user account #{merchant.errors.messages}"
+        redirect_to merchants_path
+        return
       end
     end
+
+    session[:user_id] = merchant.id
+    redirect_to root_path
   end
 
   def index
@@ -24,10 +31,10 @@ class MerchantsController < ApplicationController
   def show
   end
 
-  def destroy
+  def logout
     session[:user_id] = nil
-    flash[:success] = "Successfully logged out!"
-
+    flash[:status] = :success
+    flash[:result_text] = "Successfully logged out!"
     redirect_to root_path
   end
 end
