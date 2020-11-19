c37378f41d17cc92ba3a10a9c618663f8229b823
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-18 16:08:38 -0800

    add Merchant login and create actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+
+  def render_404
+    return render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+  end
+
+  private
+
+  def find_merchant
+    if session[:user_id]
+      @login_user = Merchant.find_by(id: session[:user_id])
+    end
+  end
+
+  def require_login
+    if @login_user.nil?
+      flash[:error] = "You must be logged in to do that"
+      redirect_to root_path
+    end
+  end
 end
