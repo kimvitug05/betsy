436f4428c65f85ae55b68094e123649d46341b75
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-19 10:32:57 -0800

    refined routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,4 @@
 Rails.application.routes.draw do
-  get 'homepages/index'
-  get 'product/index'
-  get 'product/show'
-  get 'product/new'
-  get 'product/edit'
-  get 'product/create'
-  get 'product/update'
-  get 'product/destroy'
-  get 'merchants/index'
-  get 'merchants/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmlget "/auth/github", as: "github_login" #OmniAuth login
 
   get "/auth/github", as: "github_login" #OmniAuth login
