4c746b304f7fbd9dbf0518df8a5a1450b9b4f74b
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-18 21:20:49 -0800

    1 model test written

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,30 @@
 require "test_helper"
 
 describe Product do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'validations' do
+    before do
+      merchant_id = Merchant.new(username:"123", email:"abc", id: 5)
+      @product = Product.create(name: "Shoyru", price: 5.00, merchant_id: merchant_id[:id], quantity: 500, category_id: 1)
+    end
+
+      it "is valid when all fields are filled" do
+
+        result = @product.valid?
+        p @product
+       expect(result).must_equal true
+      end
+  end
 end
+# "username"
+# t.string "email"
+# t.datetime "created_at", precision: 6, null: false
+# t.datetime "updated_at", precision: 6, null: false
+# t.string "uid"
+# t.string "provider"
+# t.string "avatar"
+
+# t.string "name"
+# t.float "price"
+# t.integer "merchant_id"
+# t.integer "quantity"
+# t.integer "category_id"
