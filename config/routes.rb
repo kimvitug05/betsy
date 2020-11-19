f943f772c45a64c83f66d1922475c2b61e6189ef
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-18 17:00:04 -0800

    add homepage route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'homepages/index'
   get 'product/index'
   get 'product/show'
   get 'product/new'
@@ -9,10 +10,14 @@ Rails.application.routes.draw do
   get 'merchants/index'
   get 'merchants/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.htmlget "/auth/github", as: "github_login" #OmniAuth login
+
   get "/auth/github", as: "github_login" #OmniAuth login
   get "/auth/:provider/callback", to: "merchants#create" #OmniAuth Github callback
   post "/logout", to: "merchants#logout", as: "logout"
 
+  root to: "homepages#index"
+
+  resources :homepages, only: [:index]
   resources :merchants
   resources :products
   resources :categorizations
