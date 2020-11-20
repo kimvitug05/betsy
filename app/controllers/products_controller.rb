969813e2dbfce737777dcd52d41ca9ff4c1003da
Madeline <madelign@gmail.com>
2020-11-20 15:08:18 -0500

    products controller/testing in progress

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
-  before_action :find_product, only: [:show, :edit, :update]
+  before_action :find_product, only: [:show, :edit, :update, :create]
+  before_action :find_merchant, only: [:edit, :update, :create]
   # skip_before_action :require_login, except [:new, :edit, :destroy]
 
   def index
@@ -7,7 +8,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-    render_404 unless @product
+    # render_404 unless @product
   end
 
   def cart
@@ -75,13 +76,16 @@ class ProductsController < ApplicationController
     if @product.save
       flash[:status] = :success
       flash[:result_text] = "Successfully created new product: #{@product.name}"
-      redirect_to product_path(@product)
+      redirect_to product_path(@product.id)
     else
       flash[:status] = :failure
       flash[:result_text] = "Could not create #{@product}"
       flash[:messages] = @product.errors.messages
       render :new, status: :bad_request
     end
+  end
+
+  def edit
   end
 
   def update
@@ -117,7 +121,17 @@ class ProductsController < ApplicationController
   end
 
   def find_product
-    @product = Product.find_by(id: params[:id])
+    @product = Product.find_by_id(id: params[:id])
+    return render_404 unless @product
+  end
+
+  def find_merchant
+    @merchant = Merchant.find_by(id: params[:author_id])
+
+    if @merchant.nil?
+      flash.now[:error] = "Merchant not found."
+      render :new, status: :bad_request
+    end
   end
 
 end
