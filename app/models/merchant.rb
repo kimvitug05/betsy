cd7efd221312456ad89925291afe9d2eb00197a1
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-19 19:25:03 -0800

    write custom model methods for Merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -16,5 +16,65 @@ class Merchant < ApplicationRecord
     # We'll choose to do the saving outside of this method
     return merchant
   end
+
+  def total_revenue
+    return 0 if self.products.empty?
+    result = 0
+
+    self.products.sum do |product|
+      product.order_items.each do |order_item|
+        result += order_item.quantity * product.price
+      end
+    end
+
+    return result
+  end
+
+  def total_revenue_by_status(status)
+    return 0 if self.products.empty?
+    result = 0
+
+    self.products.each do |product|
+      product.order_items.each do |order_item|
+        if order_item.order.status == status
+          result += order_item.quantity * product.price
+        end
+      end
+    end
+
+    return result
+  end
+
+  #TODO is this a quantity or a list of orders?
+
+  def total_num_orders_by_status(status)
+    return 0 if self.products.empty?
+    result = []
+
+    self.products.each do |product|
+      product.order_items.each do |order_item|
+        if order_item.order.status == status
+          result << order_item.order_id
+        end
+      end
+    end
+
+    return result.uniq.length
+  end
+
+  def filter_orders(status)
+    return 0 if self.products.empty?
+    result = []
+
+    self.products.each do |product|
+      product.order_items.each do |order_item|
+        if order_item.order.status == status
+          result << order_item.order
+        end
+      end
+    end
+
+    return result.uniq
+  end
 end
 
