216b10a980bbd5e84f7992d164edde9415b599d8
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-20 21:57:56 -0800

    add partial for product-card and product grid

--- app/controllers/categorizations_controller.rb
+++ app/controllers/categorizations_controller.rb
@@ -1,2 +1,41 @@
 class CategorizationsController < ApplicationController
+
+  def index
+    @categorizations = Categorization.all
+  end
+
+  def show
+    @categorization = Categorization.find_by(id: params[:id])
+
+    if @categorization.nil?
+      redirect_to categorizations_path
+      return
+    end
+  end
+
+  def new
+    @categorization = Categorization.new
+  end
+
+  def create
+    @categorization = Categorization.new(categorization_params)
+
+    if @categorization.save
+      flash[:success] = "Successfully created #{@categorization.name} #{@categorization.id}"
+      redirect_to categorization_path(@categorization.id)
+    else
+      flash.now[:warning] = "A problem occurred: Could not create #{@categorization.name}"
+      render :new, status: :bad_request
+    end
+  end
+
+  private
+
+  def categorization_params
+    return params.require(:categorization).permit(:name)
+  end
+
+  def find_categorization
+    @categorization = Categorization.find_by_id(params[:id])
+  end
 end
