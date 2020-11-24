7d1544e56e253ad14e2a7e50111960a00dcdaf2b
Madeline <madelign@gmail.com>
2020-11-24 01:12:26 -0500

    added review index, show, and new methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
-end
+  def index
+    if params[:id]
+      product = Product.find_by(id: params[:id])
+      @reviews = product.reviews
+    else
+      @reviews = Review.all
+    end
+
+    def show
+      if @review.nil?
+        redirect_to products_path
+        return
+      end
+    end
+
+    def new
+      @review = Review.new
+      @quantity = @product.quantity
+    end
+
+    private
+
+    def review_params
+      return params.require(:review).permit(:product_id, :rating)
+    end
+  end
