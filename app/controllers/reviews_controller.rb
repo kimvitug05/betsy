50c043e7721333d1746dc461ba5ad16bb8225f29
Madeline <madelign@gmail.com>
2020-11-24 12:24:39 -0500

    added create method and new form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,13 @@
 class ReviewsController < ApplicationController
+  before_action :find_product, only: [:index, :create]
+
   def index
     if params[:id]
       product = Product.find_by(id: params[:id])
       @reviews = product.reviews
     else
       @reviews = Review.all
+    end
     end
 
     def show
@@ -16,12 +19,31 @@ class ReviewsController < ApplicationController
 
     def new
       @review = Review.new
-      @quantity = @product.quantity
     end
+
+    def create
+      @review = Review.new(review_params)
+
+      if @review.save
+        flash[:status] = :success
+        flash[:result_text] = "Successfully reviewed product: #{@product.name}"
+        redirect_to product_path(@product.id)
+      else
+        flash[:status] = :failure
+        flash[:result_text] = "Could not review #{@product}"
+        flash[:messages] = @product.errors.messages
+        render :new, status: :bad_request
+      end
+    end
+
 
     private
 
+    def find_product
+      @product = Product.find_by_id(params[:id])
+    end
+
     def review_params
-      return params.require(:review).permit(:product_id, :rating)
+      return params.require(:review).permit(:rating, :description, product_id: @product)
     end
   end
