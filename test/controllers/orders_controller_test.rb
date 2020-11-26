b7f6750322799b09fa2e0bb2b36e91b449dee441
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-25 16:40:34 -0800

    tests still failing :(

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,20 +1,52 @@
 require "test_helper"
 
 describe OrdersController do
+  before do
+    @product = products(:blue_shoes)
+    @order = orders(:order1)
 
-  it "validates customer information when trying to pay for order" do
+    @order_item_hash = {
+        order_item: {
+            product_id: @product.id,
+            selected_quantity: 1,
+            order_id: @order.id
+        },
+    }
+  end
+
+  it "will not process an order with bad customer data" do
+    # bad_order = Order.create(name: "Im a terrible order", email: "kayla@kayla.com", address: nil, zip_code: nil, credit_card: nil, exp_date: nil)
+    #
+    # bad_order_item_hash = {
+    #     order_item: {
+    #         product_id: @product.id,
+    #         selected_quantity: 1,
+    #         order_id: bad_order.id
+    #     },
+    # }
+    #
+    # expect {
+    #   post submit_order_path(@product), params: bad_order_item_hash[:bad_order]
+    # }.wont_change "Order.count"
+    # # pp "This is my bad #{bad_order.name}"
+
 
   end
 
   it "changes status to paid once order is submitted" do
+    #Arrange
+    post add_to_cart_path(@product), params: @order_item_hash[:order_item]
+
+    #Assert
+    expect @order.status == "paid"
 
   end
 
   it "can remove items from the cart" do
 
-    # expect {
-    #   get clear_cart_path(@product), params: @order_item_hash[:order_item]
-    # }.wont_change "OrderItem.count"
+    expect {
+      get clear_cart_path(@product), params: @order_item_hash[:order_item]
+    }.wont_change "OrderItem.count"
 
   end
 end
